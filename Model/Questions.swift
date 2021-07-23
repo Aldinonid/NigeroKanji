e3a89f980d74281be4961089d04733bf4345a782
Aldino Efendi <aldino.efendi7@gmail.com>
2021-07-23 11:25:48 +0700

    Add listing kanji and questions

--- Model/Questions.swift
+++ Model/Questions.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+struct Question {
+	let message: String
+	let answer: String
+}
+
+struct Kanji {
+	let name: String
+}
+
+var questionList: [Question] = [
+	Question(message: "Which one of the kanji means meeting?", answer: "会"),
+	Question(message: "Which one of the kanji means music?", answer: "楽"),
+	
+]
+
+
+var kanjiList: [Kanji] = [
+	Kanji(name: "会"),
+	Kanji(name: "楽"),
+	
+]
