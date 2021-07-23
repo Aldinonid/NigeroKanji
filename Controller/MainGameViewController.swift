0d232c9ee5bba338479ed064939c868ac0549cf9
Aldino Efendi <aldino.efendi7@gmail.com>
2021-07-23 11:23:32 +0700

    Initial Commit

---
+++ Controller/MainGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainGameViewController.swift
+//  NigeroKanji
+//
+//  Created by Aldino Efendi on 2021/07/23.
+//
+
+import UIKit
+
+class MainGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ Model/Questions.swift
@@ -0,0 +1,8 @@
+//
+//  Questions.swift
+//  NigeroKanji
+//
+//  Created by Aldino Efendi on 2021/07/23.
+//
+
+import Foundation
